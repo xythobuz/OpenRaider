7a07f8801e4273fdef7ba59fa3f66b5c2ed1b8f5
Thomas Buck <xythobuz@xythobuz.de>
2013-12-27 20:00:10 +0100

    Bumped build id

--- mac_dist/archive.sh
+++ mac_dist/archive.sh
@@ -2,5 +2,5 @@
 
 echo Creating ZIP file..
 cd bin
-zip -r OpenRaider.zip OpenRaider.app ../COPYING ../README.md ../README.old
+zip -r OpenRaider.zip OpenRaider.app ../BUGS ../ChangeLog ../COPYING ../README.md ../README.old ../Requirements ../TODO
 echo Archive ready!
