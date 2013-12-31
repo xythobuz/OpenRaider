6240595d05e7000372054f13cb6458f8147acd2f
Thomas Buck <xythobuz@xythobuz.de>
2013-12-31 19:28:04 +0100

    Empty pages branch

--- mac_dist/archive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo Creating ZIP file..
-cd bin
-zip -r OpenRaider.zip OpenRaider.app ../BUGS ../ChangeLog ../COPYING ../README.md ../README.old ../Requirements ../TODO
-echo Archive ready!
