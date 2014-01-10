a73a64e949a594b2634f3bb9a89cd4fc0f50962b
Thomas Buck <xythobuz@xythobuz.de>
2014-01-10 01:04:33 +0100

    Added greatest

--- test/Sound.cpp
+++ test/Sound.cpp
@@ -8,64 +8,73 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <greatest.h>
 #include <Sound.h>
 
-/*!
- * \brief Sound Unit Test
- * \param argc Number of arguments. Has to be > 1
- * \param argv Argument array. argv[1] will be played as wav file
- * \returns always zero
- */
-int main(int argc, char* argv[])
-{
-	Sound snd;
-	FILE *f;
-	unsigned int l;
-	unsigned char *buf;
-   int id, ret;
+#define TESTFILE "data/sample.wav"
 
+Sound sound;
+bool inited = false;
 
-	if (argc > 1)
-	{
-		snd.init();
-		printf("Loading %s\n", argv[1]);
-		ret = snd.addFile(argv[1], &id, snd.SoundFlagsNone);
-		printf("Load returned %i\n", ret);
-		printf("Playing %u::%s\n", id, argv[1]);
-		snd.play(id);
+GREATEST_MAIN_DEFS();
 
-		printf("Waiting...\n");
-		sleep(1);
+TEST initSound() {
+    int ret;
+    if ((ret = sound.init()) != 0) {
+        printf("Couldn't initialize Sound (%d)!", ret);
+        FAIL();
+    }
+    inited = true;
+    PASS();
+}
 
-		f = fopen(argv[1], "rb");
+TEST playFile() {
+    int id, ret;
+    ASSERT_EQm("Sound not initialized!", inited, true);
+    if ((ret = sound.addFile(TESTFILE, &id, sound.SoundFlagsNone)) != 0) {
+        printf("Couldn't add file (%d)!", ret);
+        FAIL();
+    }
+    sound.play(id);
+    sleep(2);
+    PASS();
+}
 
-		if (f)
-		{
-			fseek(f, 0, SEEK_END);
-			l = ftell(f);
-			fseek(f, 0, SEEK_SET);
+TEST loadFile() {
+    int id, ret;
+    unsigned int l;
+    unsigned char *buf;
+    FILE *f;
+    ASSERT_EQm("Sound not initialized!", inited, true);
+    if (!(f = fopen(TESTFILE, "rb"))) {
+        printf("Couldn't open %s!", TESTFILE);
+        FAIL();
+    }
+    fseek(f, 0, SEEK_END);
+	l = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	buf = new unsigned char[l];
+	fread(buf, l, 1, f);
+	fclose(f);
+	if ((ret = sound.addWave(buf, l, &id, sound.SoundFlagsNone)) != 0) {
+        printf("Couldn't add buffer (%d)!", ret);
+        FAIL();
+    }
+	sound.play(id);
+	delete [] buf;
+	sleep(2);
+    PASS();
+}
 
-			buf = new unsigned char[l];
-			fread(buf, l, 1, f);
+SUITE(soundSuite) {
+    RUN_TEST(initSound);
+    RUN_TEST(playFile);
+    RUN_TEST(loadFile);
+}
 
-			fclose(f);
-
-			printf("Loading buffer of %s\n", argv[1]);
-			ret = snd.addWave(buf, l, &id, snd.SoundFlagsNone);
-			printf("Load returned %i\n", ret);
-			printf("Playing buffer of %u::%s\n", id, argv[1]);
-			snd.play(id);
-
-			delete [] buf;
-			printf("Waiting...\n");
-			sleep(1);
-		}
-	}
-	else
-	{
-		printf("%s filename.wav\n", argv[0]);
-	}
-
-	return 0;
+int main(int argc, char *argv[]) {
+    GREATEST_MAIN_BEGIN();
+    RUN_SUITE(soundSuite);
+    GREATEST_MAIN_END();
 }
 
