ecac3d4abfae7b864c0a968dc482d612232976f1
Thomas Buck <xythobuz@xythobuz.de>
2014-01-10 18:34:34 +0100

    Removed endian files

--- include/Light.h
+++ include/Light.h
@@ -1,68 +1,33 @@
-/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: t; c-basic-offset: 3 -*- */
-/*================================================================
+/*! \file include/Light.h
+ * \brief The GL light class
  *
- * Project : Freyja
- * Author  : Terry 'Mongoose' Hendrix II
- * Website : http://www.westga.edu/~stu7440/
- * Email   : stu7440@westga.edu
- * Object  : Light
- * License : No use w/o permission (C) 2002 Mongoose
- * Comments: This is the GL light class
- *
- *
- *           This file was generated using Mongoose's C++
- *           template generator script.  <stu7440@westga.edu>
- *
- *-- History ------------------------------------------------
- *
- * 2002.01.27:
- * Mongoose - Created
- ================================================================*/
+ * \author Mongoose
+ */
 
-
-#ifndef __FREYJA_MONGOOSE_LIGHT_H_
-#define __FREYJA_MONGOOSE_LIGHT_H_
+#ifndef _LIGHT_H_
+#define _LIGHT_H_
 
 #include <MatMath.h>
 
+class Light {
+public:
+    typedef enum {
+        typePoint = 1,
+        typeSpot = 2,
+        typeDirectional = 3
+    } LightType;
 
-class Light
-{
- public:
+    // These aren't used anywhere? -- xythobuz
+    //float mAmbient[4]; //! Ambient color
+    //float mDiffuse[4]; //! Diffuse color
+    //float mSpecular[4]; //! Specular color
 
-	typedef enum
-   {
-		typePoint = 1,
-		typeSpot = 2,
-		typeDirectional = 3
-	} FreyjaLightType;
-
-
-	float mAmbient[4];          /* Ambient color */
-
-	float mDiffuse[4];          /* Diffuse color */
-
-	float mSpecular[4];         /* Specular color */
-
-	vec4_t mPos;                /* Light position in 3 space */
-
-	vec3_t mDir;                /* Light direction */
-
-	float mAtt;                 /* */
-
-	vec4_t mColor;              /* Color of light */
-
-	vec_t mCutoff;              /* Fade out distance */
-
-	FreyjaLightType mType;	    /* Type of light */
-
- private:
-
-	//unsigned int mFlags;        /* Flags for this light */
-
-	//unsigned int mId;           /* Unique identifier of this light */
-
-	//char *mName;                /* Light name */
+    vec4_t mPos; //! Light position in 3 space
+    vec3_t mDir; //! Light direction
+    float mAtt;
+    vec4_t mColor; //! Color of light
+    vec_t mCutoff; //! Fade out distance
+    LightType mType; //! Type of light
 };
 
 #endif
