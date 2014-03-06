8058f5c6a9cecf12e54af820520045f4c0eabf6f
Thomas Buck <xythobuz@xythobuz.de>
2014-03-06 18:36:56 +0100

    Moved bounding volumes into ViewVolume

--- include/ViewVolume.h
+++ include/ViewVolume.h
@@ -9,7 +9,33 @@
 #define _VIEWVOLUME_H_
 
 #include "Matrix.h"
-#include "BoundingVolume.h"
+
+/*!
+ * \brief Defines a 3D sphere.
+ */
+class BoundingSphere {
+public:
+    vec3_t mCenter; //!< Center of bounding sphere
+    vec_t mRadius; //!< Raduis of bounding sphere
+};
+
+/*!
+ * \brief Defines a 3D rectangle.
+ */
+class BoundingBox {
+public:
+    vec3_t mMin; //!< Bounding box MIN point
+    vec3_t mMax; //!< Bounding box MAX point
+};
+
+/*!
+ * \brief Defines a 3D sphere and/or rectangle.
+ */
+class BoundingVolume {
+public:
+    BoundingSphere mSphere; //!< Bounding sphere of this volume
+    BoundingBox mBox; //!< Bounding box of this volume
+};
 
 /*!
  * \brief Viewing Volume for culling use
