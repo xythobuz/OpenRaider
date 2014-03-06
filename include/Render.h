90351b74922e19b99d24699ded35d463b192d58b
Thomas Buck <xythobuz@xythobuz.de>
2014-03-06 18:42:17 +0100

    Moved Light into Render

--- include/Render.h
+++ include/Render.h
@@ -13,7 +13,6 @@
 #include "Vector.h"
 #include "Matrix.h"
 #include "ViewVolume.h"
-#include "Light.h"
 #include "World.h"
 #include "SkeletalModel.h"
 #include "Mesh.h"
@@ -24,6 +23,33 @@
 #ifdef USING_EMITTER
 #include "Emitter.h"
 #endif
+
+/*!
+ * \brief The GL light class
+ */
+class Light {
+public:
+    /*!
+     * \brief Type a light can be of
+     */
+    typedef enum {
+        typePoint       = 1, //!< Point light
+        typeSpot        = 2, //!< Spot light
+        typeDirectional = 3  //!< Directional light
+    } LightType;
+
+    // These aren't used anywhere? -- xythobuz
+    //float mAmbient[4]; //! Ambient color
+    //float mDiffuse[4]; //! Diffuse color
+    //float mSpecular[4]; //! Specular color
+
+    vec4_t mPos; //! Light position in 3 space
+    vec3_t mDir; //! Light direction
+    float mAtt;
+    vec4_t mColor; //! Color of light
+    vec_t mCutoff; //! Fade out distance
+    LightType mType; //! Type of light
+};
 
 /*!
  * \brief RenderRoom used by Renderer
