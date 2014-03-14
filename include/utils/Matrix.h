cf3e00c3d62eb5c833066aa08135ae8d46dbadbc
Thomas Buck <xythobuz@xythobuz.de>
2014-03-14 20:33:23 +0100

    Added Matrix and Vector3d into utils

---
+++ include/utils/Matrix.h
@@ -0,0 +1,201 @@
+/*!
+ * \file include/utils/Matrix.h
+ * \brief 3D Matrix
+ *
+ * \author Mongoose
+ */
+
+#ifndef _UTILS_MATRIX_H_
+#define _UTILS_MATRIX_H_
+
+#include "utils/math.h"
+#include "utils/Quaternion.h"
+#include "utils/Vector3d.h"
+
+
+/*!
+ * \brief 3D Matrix
+ *
+ * Multidim map for row order encoding
+ *
+ *     ///////////////////////////////////////////////
+ *     // 0,0 - 0;   0,1 - 1;   0,2 - 2;   0,3 - 3  //
+ *     // 1,0 - 4;   1,1 - 5;   1,2 - 6;   1,3 - 7  //
+ *     // 2,0 - 8;   2,1 - 9;   2,2 - 10;  2,3 - 11 //
+ *     // 3,0 - 12;  3,1 - 13;  3,2 - 14;  3,3 - 15 //
+ *     ///////////////////////////////////////////////
+ *
+ * Multidim map for column order encoding
+ *
+ *     ///////////////////////////////////////////////
+ *     // 0,0 - 0;   0,1 - 4;   0,2 - 8;   0,3 - 12 //
+ *     // 1,0 - 1;   1,1 - 5;   1,2 - 9;   1,3 - 13 //
+ *     // 2,0 - 2;   2,1 - 6;   2,2 - 10;  2,3 - 14 //
+ *     // 3,0 - 3;   3,1 - 7;   3,2 - 11;  3,3 - 15 //
+ *     ///////////////////////////////////////////////
+ */
+class Matrix {
+public:
+
+    /*!
+     * \brief Constructs an object of Matrix
+     */
+    Matrix();
+
+    /*!
+     * \brief Constructs an object of Matrix
+     * \param mat Matrix as data source
+     */
+    Matrix(matrix_t mat);
+
+    /*!
+     * \brief Constructs an object of Matrix
+     * \param q Converts and assigns the Quaternion to the Matrix
+     */
+    Matrix(Quaternion &q);
+
+    /*!
+     * \brief Returns this matrix copy
+     * \param mat target
+     */
+    void getMatrix(matrix_t mat);
+
+    /*!
+     * \brief Returns this matrix transposed
+     * \param mat target
+     */
+    void getTransposeMatrix(matrix_t mat);
+
+    /*!
+     * \brief Returns this matrix inverted
+     * \param mat target
+     */
+    bool getInvert(matrix_t mat);
+
+    /*!
+     * \brief Multiplies two matrices
+     * \param a first matrix
+     * \param b second matrix
+     * \returns resultant matrix
+     */
+    static Matrix multiply(const Matrix &a, const Matrix &b);
+
+    /*!
+     * \brief Multiplies v vector and this matrix
+     * \param v vector
+     * \param result where the result will be stored, may be same as v
+     */
+    void multiply4v(vec4_t v, vec4_t result);
+
+    /*!
+     * \brief Multiplies v vector and this matrix
+     * \param v vector
+     * \param result where the result will be stored, may be same as v
+     */
+    void multiply3v(vec3_t v, vec3_t result);
+
+    /*!
+     * \brief Prints matrix values to stdout
+     */
+    void print();
+
+    /*!
+     * \brief Is this matrix the identity matrix?
+     * \returns true if it is identity, false otherwise
+     */
+    bool isIdentity();
+
+    /*!
+     * \brief Multiplies a and this matrix
+     * \param a matrix to multiply with
+     * \returns resultant matrix
+     */
+    Matrix operator *(const Matrix &a);
+
+    /*!
+     * \brief Multiply vector by this matrix
+     * \param v Vector to multiply with
+     * \returns resultant vector (mult)
+     */
+    Vector3d operator *(Vector3d v);
+
+    /*!
+     * \brief Sets to identity matrix
+     */
+    void setIdentity();
+
+    /*!
+     * \brief S et the matrix
+     * \fixme dangerous, scary, boo!
+     * \param mat new matrix
+     */
+    void setMatrix(matrix_t mat);
+
+    /*!
+     * \brief Rotate object in 3D space
+     * \param x x rotation in radians
+     * \param y y rotation in radians
+     * \param z z rotation in radians
+     */
+    void rotate(vec_t x, vec_t y, vec_t z);
+
+    /*!
+     * \brief Rotate object in 3D space
+     * \param xyz rotation in radians
+     */
+    void rotate(const vec_t *xyz);
+
+    /*!
+     * \brief Scale object in 3D space
+     * \param x x scaling
+     * \param y y scaling
+     * \param z z scaling
+     */
+    void scale(vec_t x, vec_t y, vec_t z);
+
+    /*!
+     * \brief Scale object in 3D space
+     * \param xyz scaling factors
+     */
+    void scale(const vec_t *xyz);
+
+    /*!
+     * \brief Translate (move) object in 3D space
+     * \param x x translation
+     * \param y y translation
+     * \param z z translation
+     */
+    void translate(vec_t x, vec_t y, vec_t z);
+
+    /*!
+     * \brief Translate (move) object in 3D space
+     * \param xyz translations
+     */
+    void translate(const vec_t *xyz);
+
+    /*!
+     * \brief Transpose this matrix
+     */
+    void transpose();
+
+    matrix_t mMatrix; //!< Data model, moved public for faster external renderer feedback use
+
+private:
+
+    /*!
+     * \brief Copys value from source to dest
+     * \param source source
+     * \param dest destination
+     */
+    static void copy(matrix_t source, matrix_t dest);
+
+    /*!
+     * \brief Multiplies matrices a and b. Neither a or b is also the result.
+     * \param a first matrix
+     * \param b second matrix
+     * \param result wil be set to resultant matrix value
+     */
+    static void multiply(const matrix_t a, const matrix_t b, matrix_t result);
+};
+
+#endif
---
+++ src/utils/Vector3d.cpp
@@ -0,0 +1,135 @@
+/*!
+ * \file src/utils/Vector3d.cpp
+ * \brief 3D Math vector
+ *
+ * \author Mongoose
+ */
+
+#include <math.h>
+
+#include "utils/Vector3d.h"
+
+Vector3d::Vector3d() {
+    mVec[0] = mVec[1] = mVec[2] = 0.0f;
+}
+
+Vector3d::Vector3d(vec3_t v) {
+    mVec[0] = v[0];
+    mVec[1] = v[1];
+    mVec[2] = v[2];
+}
+
+Vector3d::Vector3d(vec_t x, vec_t y, vec_t z) {
+    mVec[0] = x;
+    mVec[1] = y;
+    mVec[2] = z;
+}
+
+Vector3d::Vector3d(const Vector3d &v) {
+    mVec[0] = v.mVec[0];
+    mVec[1] = v.mVec[1];
+    mVec[2] = v.mVec[2];
+}
+
+vec_t Vector3d::dot(const Vector3d &u, const Vector3d &v) {
+    return (u.mVec[0]*v.mVec[0] + u.mVec[1]*v.mVec[1] + u.mVec[2]*v.mVec[2]);
+}
+
+Vector3d Vector3d::cross(const Vector3d &u, const Vector3d &v) {
+    return Vector3d(u.mVec[1] * v.mVec[2] - u.mVec[2] * v.mVec[1],
+            u.mVec[2] * v.mVec[0] - u.mVec[0] * v.mVec[2],
+            u.mVec[0] * v.mVec[1] - u.mVec[1] * v.mVec[0]);
+}
+
+vec_t Vector3d::magnitude() {
+    return sqrtf(mVec[0]*mVec[0] + mVec[1]*mVec[1] + mVec[2]*mVec[2]);
+}
+
+Vector3d Vector3d::unit() {
+    vec_t norm = magnitude();
+
+    return Vector3d(mVec[0] / norm,
+            mVec[1] / norm,
+            mVec[2] / norm);
+}
+
+Vector3d Vector3d::zeroVector() {
+    return Vector3d(0, 0, 0);
+}
+
+Vector3d Vector3d::operator +(const Vector3d &v) {
+    return Vector3d(mVec[0] + v.mVec[0],
+            mVec[1] + v.mVec[1],
+            mVec[2] + v.mVec[2]);
+}
+
+Vector3d Vector3d::operator -(const Vector3d &v) {
+    return Vector3d(mVec[0] - v.mVec[0],
+            mVec[1] - v.mVec[1],
+            mVec[2] - v.mVec[2]);
+}
+
+Vector3d Vector3d::operator -() {
+    return Vector3d(-mVec[0],
+            -mVec[1],
+            -mVec[2]);
+}
+
+Vector3d Vector3d::operator *(vec_t s) {
+    return Vector3d(s * mVec[0],
+            s * mVec[1],
+            s * mVec[2]);
+}
+
+Vector3d Vector3d::operator /(vec_t s) {
+    return Vector3d(mVec[0] / s,
+            mVec[1] / s,
+            mVec[2] / s);
+}
+
+vec_t Vector3d::operator *(const Vector3d &v) {
+    return dot(*this, v);
+}
+
+void Vector3d::normalize() {
+    vec_t norm = magnitude();
+
+    mVec[0] /= norm;
+    mVec[1] /= norm;
+    mVec[2] /= norm;
+}
+
+void Vector3d::zero() {
+    mVec[0] = 0;
+    mVec[1] = 0;
+    mVec[2] = 0;
+}
+
+Vector3d &Vector3d::operator =(const Vector3d &v) {
+    mVec[0] = v.mVec[0];
+    mVec[1] = v.mVec[1];
+    mVec[2] = v.mVec[2];
+    return *this;
+}
+
+Vector3d &Vector3d::operator +=(const Vector3d &v) {
+    mVec[0] += v.mVec[0];
+    mVec[1] += v.mVec[1];
+    mVec[2] += v.mVec[2];
+    return *this;
+}
+
+Vector3d &Vector3d::operator -=(const Vector3d &v) {
+    mVec[0] -= v.mVec[0];
+    mVec[1] -= v.mVec[1];
+    mVec[2] -= v.mVec[2];
+    return *this;
+}
+
+Vector3d &Vector3d::operator *=(vec_t s) {
+    mVec[0] *= s;
+    mVec[1] *= s;
+    mVec[2] *= s;
+    return *this;
+}
+
