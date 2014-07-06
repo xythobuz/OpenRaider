925c62cc2f652b61db42dc00ed5a7e1efab2bd6f
Thomas Buck <xythobuz@xythobuz.de>
2014-07-07 00:17:29 +0200

    RANDR extension

--- cmake/travis_after_success_linux.sh
+++ cmake/travis_after_success_linux.sh
@@ -6,15 +6,14 @@ cd src
 echo "set basedir \".\"" > OpenRaider.ini
 echo "set pakdir \".\"" >> OpenRaider.ini
 echo "set audiodir \".\"" >> OpenRaider.ini
-echo "set datadir \".\"" >> OpenRaider.ini
+echo "set datadir \"../../data\"" >> OpenRaider.ini
 echo "set font \"../../data/test.ttf\"" >> OpenRaider.ini
 
-xvfb-run -a -s "-screen 0 640x480x24" ./OpenRaider &
+xvfb-run -a -s "+extension RANDR -screen 0 640x480x24" ./OpenRaider &
 sleep 5 # Wait for OpenRaider to start
 
 import -window root screenshot.png
 
 # Find a better way for this!
-sudo apt-get install uuencode mail
-uuencode screenshot.png | mail -s "Screenshot" xythobuz@xythobuz.de
+ls -la
 
