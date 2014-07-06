26e6a30ac136d6d88edd705c43a2eb78ec5098b4
Thomas Buck <xythobuz@xythobuz.de>
2014-07-07 01:04:53 +0200

    IRC notification

--- cmake/travis_after_success_linux.sh
+++ cmake/travis_after_success_linux.sh
@@ -15,5 +15,15 @@ import -window root -display :99.0 screenshot.png
 
 wget http://imgur.com/tools/imgurbash.sh
 chmod a+x ./imgurbash.sh
-./imgurbash.sh screenshot.png
+./imgurbash.sh screenshot.png > url
+
+echo 'USER OR-Travis-Worker OR-Travis-Worker OR-Travis-Worker OR-Travis-Worker' > msg
+echo 'NICK OR-Travis-Worker' >> msg
+echo 'JOIN #OpenRaider' >> msg
+echo "PRIVMSG #OpenRaider Travis $CXX Worker has a screenshot:" >> msg
+echo -n "PRIVMSG #OpenRaider " >> msg
+cat url >> msg
+echo 'QUIT' >> msg
+
+cat msg | nc chat.freenode.net 6667
 
