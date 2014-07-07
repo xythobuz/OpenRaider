ed9046584c85920b36ab25f38f8550484b2c249d
Thomas Buck <xythobuz@xythobuz.de>
2014-07-07 11:43:57 +0200

    Try IRC notification again

--- cmake/travis_after_success_linux.sh
+++ cmake/travis_after_success_linux.sh
@@ -17,9 +17,13 @@ wget http://imgur.com/tools/imgurbash.sh
 chmod a+x ./imgurbash.sh
 ./imgurbash.sh screenshot.png > url
 
-export SHIC_SCRIPT=":j #OpenRaider; New Screenshot: $(url); :q"
-
-wget https://raw.githubusercontent.com/halhen/shic/master/shic
-chmod a+x ./shic
-./shic -h chat.freenode.net -p 6667 -n OR-Travis-Worker
+(
+    echo NICK OR-Travis-Worker
+    echo USER OR-Travis-Worker 8 \* : OR-Travis-Worker
+    sleep 2
+    echo 'JOIN #OpenRaider'
+    echo 'PRIVMSG #OpenRaider New Screenshot: ' $(cat url | head -n 1)
+    sleep 2
+    echo QUIT
+) | nc chat.freenode.net 6667
 
