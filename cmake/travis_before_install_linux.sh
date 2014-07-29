982135f14221e9df5b72a9c6e7471a3bc7d4842a
Thomas Buck <xythobuz@xythobuz.de>
2014-07-29 23:26:40 +0200

    Newer cmake for Travis Linux worker

--- cmake/travis_before_install_linux.sh
+++ cmake/travis_before_install_linux.sh
@@ -2,5 +2,6 @@
 
 sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:kalakris/cmake
 sudo apt-get -qq -y update
 
