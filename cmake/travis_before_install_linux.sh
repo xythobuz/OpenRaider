459f8275a1b27d0b5cf1c2b7fc8e348dc694bf1d
Thomas Buck <xythobuz@xythobuz.de>
2015-03-14 18:51:36 +0100

    Next Travis try

--- cmake/travis_before_install_linux.sh
+++ cmake/travis_before_install_linux.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository -y ppa:smspillaz/cmake-2.8.12
+sudo add-apt-repository -y ppa:andykimpe/cmake
 sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
 sudo apt-get -qq -y update
 
--- cmake/travis_install_linux.sh
+++ cmake/travis_install_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -qq -y install libsdl2-dev libsdl2-ttf-dev libalut-dev libpng12-dev zlib1g-dev cmake libglm-dev
+sudo apt-get -qq -y install libsdl2-dev libsdl2-ttf-dev libalut-dev libpng12-dev cmake libglm-dev
 
 if [ "$CC" = "gcc" ]; then sudo apt-get install gcc-4.8; fi
 if [ "$CXX" = "g++" ]; then sudo apt-get install g++-4.8; fi
