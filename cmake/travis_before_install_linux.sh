ca1c94a28b036052562e3172117e9c7b9b071029
Thomas Buck <xythobuz@xythobuz.de>
2015-03-19 11:54:13 +0100

    Travis libc++ ppa

--- cmake/travis_before_install_linux.sh
+++ cmake/travis_before_install_linux.sh
@@ -3,6 +3,7 @@
 sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:andykimpe/cmake
+sudo add-apt-repository -y ppa:jhe/libc++
 sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
 sudo apt-get -qq -y update
 
