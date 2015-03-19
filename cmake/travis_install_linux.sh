fb7a266979520f1e9942c016aaa1164cac2045cb
Thomas Buck <xythobuz@xythobuz.de>
2015-03-19 11:32:23 +0100

    Install libc++-dev in Travis Linux

--- cmake/travis_install_linux.sh
+++ cmake/travis_install_linux.sh
@@ -5,7 +5,7 @@ sudo apt-get -qq -y install libsdl2-dev libsdl2-ttf-dev libalut-dev libpng12-dev
 if [ "$CC" = "gcc" ]; then sudo apt-get install gcc-4.8; fi
 if [ "$CXX" = "g++" ]; then sudo apt-get install g++-4.8; fi
 
-if [ "$CXX" = "clang++" ]; then sudo apt-get install --allow-unauthenticated clang-3.5; fi
+if [ "$CXX" = "clang++" ]; then sudo apt-get install --allow-unauthenticated clang-3.5 libc++-dev; fi
 
 wget 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.6.3/glm-0.9.6.3.zip'
 unzip glm-0.9.6.3.zip
