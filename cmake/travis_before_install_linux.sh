ad05e1918e20bb4421f8eab5510bd9bab73bffcb
Thomas Buck <xythobuz@xythobuz.de>
2015-08-01 22:05:03 +0200

    Try Travis again

--- cmake/travis_before_install_linux.sh
+++ cmake/travis_before_install_linux.sh
@@ -3,7 +3,6 @@
 sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:andykimpe/cmake
-sudo add-apt-repository -y ppa:jhe/libc++
-sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
+sudo add-apt-repository -y ppa:h-rayflood/llvm
 sudo apt-get -qq -y update
 
--- cmake/travis_install_linux.sh
+++ cmake/travis_install_linux.sh
@@ -5,9 +5,18 @@ sudo apt-get -qq -y install libsdl2-dev libsdl2-ttf-dev libalut-dev libpng12-dev
 if [ "$CC" = "gcc" ]; then sudo apt-get install gcc-4.8; fi
 if [ "$CXX" = "g++" ]; then sudo apt-get install g++-4.8; fi
 
-if [ "$CXX" = "clang++" ]; then sudo apt-get install --allow-unauthenticated clang-3.5 libc++-dev; fi
+if [ "$CXX" = "clang++" ]; then sudo apt-get install --allow-unauthenticated clang-3.5; fi
 
 wget 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.6.3/glm-0.9.6.3.zip'
 unzip glm-0.9.6.3.zip
 sudo cp -R glm/glm /usr/local/include/glm
+
+if [ "$CXX" = "clang++" ]; then export CXXFLAGS="-stdlib=libc++"; fi
+if [ "$CXX" = "clang++" ]; then svn co --quiet http://llvm.org/svn/llvm-project/libcxx/trunk libcxx; fi
+if [ "$CXX" = "clang++" ]; then cd libcxx/lib && bash buildit; fi
+if [ "$CXX" = "clang++" ]; then sudo cp ./libc++.so.1.0 /usr/lib/; fi
+if [ "$CXX" = "clang++" ]; then sudo mkdir /usr/include/c++/v1; fi
+if [ "$CXX" = "clang++" ]; then cd .. && sudo cp -r include/* /usr/include/c++/v1/; fi
+if [ "$CXX" = "clang++" ]; then cd /usr/lib && sudo ln -sf libc++.so.1.0 libc++.so; fi
+if [ "$CXX" = "clang++" ]; then sudo ln -sf libc++.so.1.0 libc++.so.1 && cd $cwd; fi
 
